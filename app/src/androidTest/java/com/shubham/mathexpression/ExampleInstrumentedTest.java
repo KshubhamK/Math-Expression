cd6033f7dc50f13b8cb7251c65dde83ef0c01a15
shubham <shubham@alveo.fit>
2023-08-29 17:34:33 +0530

    >>JUnit test cases added

--- app/src/androidTest/java/com/shubham/mathexpression/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/shubham/mathexpression/ExampleInstrumentedTest.java
@@ -1,5 +1,8 @@
 package com.shubham.mathexpression;
 
+import static com.shubham.mathexpression.utils.AppConstants.COMICS_DATE;
+import static com.shubham.mathexpression.utils.AppConstants.SIMPLE_DATE;
+
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -9,6 +12,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +29,26 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.shubham.mathexpression", appContext.getPackageName());
+    }
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void convertDateFormat() {
+        String newTime;
+        SimpleDateFormat actual = new SimpleDateFormat(SIMPLE_DATE);
+        SimpleDateFormat target = new SimpleDateFormat(COMICS_DATE);
+        Date date;
+        try {
+            date = actual.parse("2022-03-03");
+            newTime = target.format(date);
+            System.out.println("newTime " + newTime);
+        } catch (ParseException e) {
+            System.out.println("exception caught exception");
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/shubham/mathexpression/utils/AppConstants.java
+++ app/src/main/java/com/shubham/mathexpression/utils/AppConstants.java
@@ -2,4 +2,6 @@ package com.shubham.mathexpression.utils;
 
 public class AppConstants {
     public static String SIMPLE_DATE = "dd MMM, yyyy | hh:mm a";
+    public static String COMICS_DATE = "dd MMM yyyy";
+
 }
